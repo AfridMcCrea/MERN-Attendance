887360a678f637188ae4251ef1fea9937da9d517
AfridMcCrea <73422624+AfridMcCrea@users.noreply.github.com>
2024-08-07 02:38:43 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import attendanceRoutes from './routes/attendance.route.js';
-
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -15,12 +15,22 @@ mongoose.connect(process.env.MONGO)
         console.log(err);
     });
 
+    const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
+
+
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/attendance', attendanceRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*' , (req , res)=>{
+    res.sendFile(path.join(__dirname, 'client' , 'dist' , 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
